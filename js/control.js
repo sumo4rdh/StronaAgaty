668ca1e95f4abf3755e7dd48348f2f99f80863a7
Artur Brzozowski <artur.brzozowski@zhp.net.pl>
2015-11-01 18:19:38 +0100

    dodany początek angulara

---
+++ js/control.js
@@ -0,0 +1,19 @@
+app.controller('MainController', ['$scope', function($scope)
+       {
+         $scope.zdjecias = [
+         				{
+         					info: 'slubne',
+         					img: 'images/z (1).jpg'
+
+         				},	
+						{
+         					info: 'slubne',
+         					img: 'images/z (2).jpg'
+
+         				},
+
+
+         ];
+					
+	
+}]);
