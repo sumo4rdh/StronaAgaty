afc54357bb50338642d96417eba998bff0ef3a11
Artur Brzozowski <artur.brzozowski@zhp.net.pl>
2015-11-01 22:39:21 +0100

    added gallery based on angular js

--- js/control.js
+++ js/control.js
@@ -6,13 +6,58 @@ app.controller('MainController', ['$scope', function($scope)
          					img: 'images/z (1).jpg'
 
          				},	
-						{
+						   {
          					info: 'slubne',
          					img: 'images/z (2).jpg'
-
          				},
-
-
+                      {
+                        info: 'slubne',
+                        img: 'images/z (3).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (4).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (5).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (6).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (7).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (8).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (9).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (10).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (11).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (12).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (13).jpg'
+                     },
+                      {
+                        info: 'slubne',
+                        img: 'images/z (14).jpg'
+                     },
          ];
 					
 	
